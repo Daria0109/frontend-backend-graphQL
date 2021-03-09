ec466ae7253644494d0a1ee474cc257dcb953008
Daria0109 <da6ulya888@gmail.com>
2021-03-09 18:11:08 +0300

    UI is created. Apollo is lounched

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -18,6 +19,7 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
+app.use(cors())
 app.listen(PORT, err => {
   err ? console.log(error) : console.log('server started!')
 })
