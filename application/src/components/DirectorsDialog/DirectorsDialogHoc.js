4862bd59455bc1c0e02206fc5f52b71926e8f387
Daria0109 <da6ulya888@gmail.com>
2021-03-10 11:37:48 +0300

    Delete mutations are added

---
+++ application/src/components/DirectorsDialog/DirectorsDialogHoc.js
@@ -0,0 +1,17 @@
+import { compose } from 'recompose';
+import {graphql} from 'react-apollo';
+
+import {deleteDirectorMutation} from './mutations';
+import {directorsQuery} from './../DirectorsTable/Queries'
+
+const withGraphqlDelete = graphql(deleteDirectorMutation, {
+  props: ({mutate}) => ({
+    deleteDirector: id => mutate({
+      variables: id,
+      refetchQueries: [{query: directorsQuery}]
+    })
+  })
+})
+
+
+export default compose(withGraphqlDelete);
